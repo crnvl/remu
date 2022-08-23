395248a99b08e50584bb1a8b2b38ecb6c0261862
yuki <angelsflyinhell@namespace.media>
2022-08-23 08:42:52 +0200

    Create CPU impl

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,3 +3,17 @@ pub struct CPU {
     pub status: u8,
     pub program_counter: u16
 }
+
+impl CPU {
+    pub fn new() -> CPU {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        todo!("")
+    }
+}
