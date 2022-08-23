93b8aed530e8104bf38f40b449597a4f0fa09173
angelsflyinhell <azami@namespace.media>
2022-08-23 16:35:21 +0200

    Update CPU to handle 3 opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,19 +1,65 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
-    pub program_counter: u16
+    pub program_counter: u16,
 }
 
 impl CPU {
     pub fn new() -> CPU {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
-            program_counter: 0
+            program_counter: 0,
         }
     }
 
     pub fn interpret(&mut self, program: Vec<u8>) {
-        todo!("")
+        self.program_counter = 0;
+
+        loop {
+            let opscode = program[self.program_counter as usize];
+            self.program_counter += 1;
+
+            match opscode {
+                0xA9 => {
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
+
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                0xAA => {
+                    self.register_x = self.register_a;
+
+                    if (self.register_x == 0) {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                0x00 => {
+                    return;
+                }
+                _ => todo!(""),
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+pub mod cpu;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::cpu;
+
+    #[test]
+    fn test_0xa9_lda_immidiate_load_data() {
+        let mut cpu = cpu::CPU::new();
+        cpu.interpret(vec![0xA9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = cpu::CPU::new();
+        cpu.interpret(vec![0xA9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
 }
