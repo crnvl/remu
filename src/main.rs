2a9b47e63358a863c2e34beb847f5e837910b951
angelsflyinhell <azami@namespace.media>
2022-08-24 23:30:26 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
     for event in event_pump.poll_iter() {
         match event {
             Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                std::process::exit(-1);
+                std::process::exit(0);
             }
             Event::KeyDown { keycode: Some(Keycode::W), .. } => {
                 cpu.mem_write(0xff, 0x77);
